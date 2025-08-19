6a684e549150597619721734034d880bcdb7c297
amir <amirtaki623@gmail.com>
2025-08-19 21:58:22 +0330

    part 9

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", (e) => {
-    let box = document.querySelectorAll(".box")
+    let boxes = document.querySelectorAll(".box")
+
+    boxes.forEach((box) => {
+        let spans = [];
+        for (let i  = 0 ; i < 40; i++){
+            let span = document.createElement("span")
+        }
+    })
+
 })
